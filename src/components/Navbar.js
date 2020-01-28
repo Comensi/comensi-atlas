d61aff59b48aa241600715eb3910ce284d2cb157
Peter <greenman@the-organization.com>
2020-01-28 10:46:03 +0000

    Now with a contact pge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -85,6 +85,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>              
+              <Link className="navbar-item" to="/contact">
+                Contact
+              </Link>              
             </div>
           </div>
         </div>

25281c52946daab0529c95f5e5ffec348eef6a74
Peter <greenman@the-organization.com>
2020-02-18 13:25:54 +0000

    disable auto redirect

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,7 +86,7 @@ module.exports = {
         path: `${__dirname}/src/intl`,
         languages: supportedLanguages,
         defaultLanguage: defaultLanguage,        
-        redirect: true,
+        redirect: false,
       },
     }, 
     {
